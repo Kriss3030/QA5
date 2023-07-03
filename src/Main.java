e6330e1307ff0125af66107fcc55e250d55ffc15
Kris <krisgvozdik@gmail.com>
2023-07-03 16:13:28 +0300

    DZ1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 7;
+        int b = 9;
+        a = b - a; //a - 2;
+        b = b - a; //b - 7;
+        a = b + a; //a - 9;
+        System.out.println(a);
+        System.out.println(b);
+
     }
 }
